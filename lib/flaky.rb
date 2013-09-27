62effe9d1b087e46eef0d1cfd85b9796241c1bf6
bootstraponline <code@bootstraponline.com>
2013-09-27 17:38:17 -0400

    Release 0.0.3

--- lib/flaky.rb
+++ lib/flaky.rb
@@ -8,7 +8,7 @@ require 'escape_utils'
 require 'posix/spawn' # http://rubygems.org/gems/posix-spawn
 
 module Flaky
-  VERSION = '0.0.2' unless defined? ::Flaky::VERSION
+  VERSION = '0.0.3' unless defined? ::Flaky::VERSION
   DATE = '2013-09-27' unless defined? ::Flaky::DATE
 
   # https://github.com/appium/ruby_lib/blob/0e203d76610abd519ba9d2fe9c14b50c94df5bbd/lib/appium_lib.rb#L24
