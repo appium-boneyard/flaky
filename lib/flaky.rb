8df71eebc6830c7d8375dfc1d2fc17e2a4829540
bootstraponline <code@bootstraponline.com>
2014-01-13 17:12:16 -0500

    Release 0.0.25

--- lib/flaky.rb
+++ lib/flaky.rb
@@ -9,8 +9,8 @@ require 'posix/spawn' # http://rubygems.org/gems/posix-spawn
 require 'digest/md5'
 
 module Flaky
-  VERSION = '0.0.24' unless defined? ::Flaky::VERSION
-  DATE = '2014-01-09' unless defined? ::Flaky::DATE
+  VERSION = '0.0.25' unless defined? ::Flaky::VERSION
+  DATE = '2014-01-13' unless defined? ::Flaky::DATE
 
   class << self; attr_accessor :no_video; end
   self.no_video = false; # set default value
