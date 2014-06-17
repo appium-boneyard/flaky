bc3375e4a8f68d632e09df981590eeeb9532c43f
bootstraponline <code@bootstraponline.com>
2014-06-17 13:37:11 -0400

    Update adb test

--- test/adb.rb
+++ test/adb.rb
@@ -3,19 +3,11 @@ require 'posix-spawn'
 
 Process::waitpid(POSIX::Spawn::spawn('killall', '-9', 'adb', :in => '/dev/null', :out => '/dev/null', :err => '/dev/null'))
 
-last_result = ''
-
 puts 'Locating device...'
 
 while `adb shell echo "ping"`.strip != 'ping'
   `adb kill-server`
-
-  result = `adb devices`
-  if result != last_result
-    puts result
-  end
-
-  last_result = result
+  `adb devices` # std err is printed
   sleep 5
 end
 
